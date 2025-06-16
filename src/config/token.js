10deaa0bd6c24e96594a2efca4153bddcde9c167
Hungdev21 <phamviethung21052004@gmail.com>
2025-06-16 20:44:07 +0700

    fix api

--- src/config/token.js
+++ src/config/token.js
@@ -1,6 +1,6 @@
 const token = localStorage.getItem('token'); // hoặc lấy từ Vuex/Pinia
 
-const res = await fetch(`${BASE_URL}/events`, {
+const res = await fetch(`${BASE_URL}api/events`, {
   method: 'GET',
   headers: {
     'Content-Type': 'application/json',
